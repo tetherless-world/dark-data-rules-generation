b7ec834fba0e110c2984c5cd5eff5b957efdbba3
anirudhprabhu <anirudh.prabhu@gmail.com>
2016-04-25 21:53:20 -0400

    Added Improved Rule Generation and automated the whole generation process

---
+++ DarkData_Python/ImprovedRuleGen.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+improvedRules = {}
+with pd.ExcelFile('TrialRules.xlsx') as xls:
+    improvedRules['Sheet1'] = pd.read_excel(xls, 'Sheet1', index_col=None, na_values=['NA'])
+
+print(improvedRules['Sheet1'])
+
+fnew = open('ImprovedRules.txt','w')
+fnew.write("@prefix dd: <http://www.purl.org/twc/ns/darkdata#>."+"\n"+
+"@prefix ddmeasurement: <http://darkdata.tw.rpi.edu/data/measurement/>."+"\n"+
+"@prefix ddtime: <http://darkdata.tw.rpi.edu/data/time-interval/>."+"\n"+
+"@prefix ddspatial:<http://darkdata.tw.rpi.edu/data/spatial-resolution/>."+"\n"+
+"@prefix ddinstrument:<http://darkdata.tw.rpi.edu/data/instrument/>."+"\n"+"\n")
+
+#print(improvedRules['Sheet1'])
+
+var = [['Measurement1','dd:measurement','ddmeasurement:'],['Measurement2','dd:measurement','ddmeasurement:'],['Instrument1','dd:instrument','ddinstrument:'],['Instrument2','dd:instrument','ddinstrument:'],['Time_Interval1','dd:timeInterval','ddtime:'],['Time_Interval2','dd:timeInterval','ddtime:'],['Spatial_Resolution1','dd:spatialResolution','ddspatial:'],['Spatial_Resolution2','dd:spatialResolution','ddspatial:'],['Processing_Level1','dd:processingLevel',''],['Processing_Level2','dd:processingLevel','']]
+events = [['Volcano','dd:candidateEvent','dd:'],['Hurricane','dd:candidateEvent','dd:'],['Flood','dd:candidateEvent','dd:'],['Fire','dd:candidateEvent','dd:']]
+
+print(var[len(var)-1])
+
+def runImprovedRules(compatibility,variable,eventType,i):
+    fnew.write("[Rule_no"+str(i)+":"+"\n")
+    for x in xrange(0,len(variable)-1,2):
+        if(not(str(improvedRules['Sheet1'][variable[x][0]][i])=='nan')):
+            fnew.write(str("(?datafield1 " + str(variable[x][1])+" " + str(variable[x][2])  +str(improvedRules['Sheet1'][variable[x][0]][i])+"),"+"\n"))
+        if(not(str(improvedRules['Sheet1'][variable[x+1][0]][i])=='nan')):
+            fnew.write(str("(?datafield2 " + str(variable[x][1])+" " + str(variable[x][2])  +str(improvedRules['Sheet1'][variable[x+1][0]][i])+"),"+"\n"))
+    for y in range(0,len(eventType)):
+        if(not(str(improvedRules['Sheet1'][eventType[y][0]][i])=='nan') and (not(str(improvedRules['Sheet1'][eventType[y][0]][i])=='0.0'))):
+            fnew.write("(?candidate "+str(eventType[y][1])+" ?event),"+"\n"+
+                 "(?event rdf:type "+ str(eventType[y][2]) +str(eventType[y][0])+"),"+"\n")
+    fnew.write("makeSkolem(?assertion, ?candidate, ?datafield1 , ?datafield2)"+"\n")
+    fnew.write("->"+"\n")
+    fnew.write("(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
+            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
+            "(?assertion dd:compatibilityValue dd:"+compatibility+"),"+"\n"+
+            "(?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
+            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
+            +"]"+"\n"
+            )
+
+#TODO How to state the different compatibilities?
+
+for i in range(0,len(improvedRules['Sheet1'])):
+    runImprovedRules('high',var,events,i)
+
--- DarkData_Python/RulesGen.py
+++ DarkData_Python/RulesGen.py
@@ -8,155 +8,51 @@ with pd.ExcelFile('DarkData_Rules_Improved_AP.xlsx') as xls:
     data['Sheet1'] = pd.read_excel(xls, 'For_Rule_Gen', index_col=None, na_values=['NA'])
     data['Sheet2'] = pd.read_excel(xls, 'Formatted_For_Evaluation', index_col=None, na_values=['NA'],parse_cols=[0,1,2,3,4,5,6,7,8,9,10,12,22],convert_float=False)
 
-def runrules(compatibility):
-    if(str(data['Sheet1']['Event+Type'][i])=='nan'):
-        if(np.isnan(data['Sheet1']['Processing+Level1'][i]) and np.isnan(data['Sheet1']['Processing+Level2'][i])):
-            f.write(str("[Rule_no"+str(i)+":"+"\n"+"(?datafield1 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement1'][i]+"),"+"\n"+
-            "(?datafield2 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement2'][i]+"),"+"\n"+
-            "(?datafield1 dd:instrument ddinstrument:"+data['Sheet1']['Instrument1'][i]+"),"+"\n"+
-            "(?datafield2 dd:instrument ddinstrument:"+data['Sheet1']['Instrument2'][i]+"),"+"\n"+
-            "(?datafield1 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval1'][i]+"),"+"\n"+
-            "(?datafield2 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval2'][i]+"),"+"\n"+
-            "(?datafield1 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution1'][i]+"),"+"\n"+
-            "(?datafield2 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution2'][i]+"),"+"\n"
-            #+"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level1'][i]))+")"+"\n"+
-            #"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level2'][i]))+")"+"\n"
-            +"makeSkolem(?assertion, ?candidate, ?datafield1 , ?datafield2)"+"\n"
-            +"->"+"\n"+
-            "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
-            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
-            "(?assertion dd:compatibilityValue dd:"+compatibility+"\n"+
-            " (?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
-            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
-            +"]"+"\n"
-            ))
-        elif(np.isnan(data['Sheet1']['Processing+Level1'][i])):
-            f.write(str("[Rule_no"+str(i)+":"+"\n"+"(?datafield1 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement1'][i]+"),"+"\n"+
-            "(?datafield2 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement2'][i]+"),"+"\n"+
-            "(?datafield1 dd:instrument ddinstrument:"+data['Sheet1']['Instrument1'][i]+"),"+"\n"+
-            "(?datafield2 dd:instrument ddinstrument:"+data['Sheet1']['Instrument2'][i]+"),"+"\n"+
-            "(?datafield1 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval1'][i]+"),"+"\n"+
-            "(?datafield2 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval2'][i]+"),"+"\n"+
-            "(?datafield1 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution1'][i]+"),"+"\n"+
-            "(?datafield2 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution2'][i]+"),"+"\n"
-            #+"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level1'][i]))+")"+"\n"+
-            +"(?datafield2 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level2'][i]))+"),"+"\n"
-            +"makeSkolem(?assertion, ?candidate, ?datafield1 , ?datafield2)"+"\n"
-            +"->"+"\n"+
-             "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
-            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
-            "(?assertion dd:compatibilityValue dd:"+compatibility+"\n"+
-            " (?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
-            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
-            +"]"+"\n"
-            ))
-        elif(np.isnan(data['Sheet1']['Processing+Level2'][i])):
-            f.write(str("[Rule_no"+str(i)+":"+"\n"+"(?datafield1 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement1'][i]+"),"+"\n"+
-            "(?datafield2 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement2'][i]+"),"+"\n"+
-            "(?datafield1 dd:instrument ddinstrument:"+data['Sheet1']['Instrument1'][i]+"),"+"\n"+
-            "(?datafield2 dd:instrument ddinstrument:"+data['Sheet1']['Instrument2'][i]+"),"+"\n"+
-            "(?datafield1 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval1'][i]+"),"+"\n"+
-            "(?datafield2 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval2'][i]+"),"+"\n"+
-            "(?datafield1 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution1'][i]+"),"+"\n"+
-            "(?datafield2 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution2'][i]+"),"+"\n"
-            +"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level1'][i]))+"),"+"\n"
-            #"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level2'][i]))+")"+"\n"
-            +"makeSkolem(?assertion, ?candidate, ?datafield1 , ?datafield2)"+"\n"
-            +"->"+"\n"+
-             "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
-            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
-            "(?assertion dd:compatibilityValue dd:"+compatibility+"\n"+
-            " (?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
-            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
-            +"]"+"\n"
-            ))
-    else:
-        if(np.isnan(data['Sheet1']['Processing+Level1'][i]) and np.isnan(data['Sheet1']['Processing+Level2'][i])):
-            f.write(str("[Rule_no"+str(i)+":"+"\n"+"(?datafield1 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement1'][i]+"),"+"\n"+
-            "(?datafield2 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement2'][i]+"),"+"\n"+
-            "(?datafield1 dd:instrument ddinstrument:"+data['Sheet1']['Instrument1'][i]+"),"+"\n"+
-            "(?datafield2 dd:instrument ddinstrument:"+data['Sheet1']['Instrument2'][i]+"),"+"\n"+
-            "(?datafield1 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval1'][i]+"),"+"\n"+
-            "(?datafield2 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval2'][i]+"),"+"\n"+
-            "(?datafield1 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution1'][i]+"),"+"\n"+
-            "(?datafield2 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution2'][i]+"),"+"\n"
-            #+"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level1'][i]))+")"+"\n"+
-            #"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level2'][i]))+")"+"\n"
-            +"(?candidate dd:candidateEvent ?event),"+"\n"+
-            "(?event rdf:type dd:"+data['Sheet1']['Event+Type'][i]+"),"+"\n"+
-            "makeSkolem(?assertion, ?candidate, ?event, ?datafield1 , ?datafield2)"+"\n"
-            +"->"+"\n"+
-             "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
-            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
-            "(?assertion dd:compatibilityValue dd:"+compatibility+"\n"+
-            " (?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
-            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
-            +"]"+"\n"
-            ))
-        elif(np.isnan(data['Sheet1']['Processing+Level1'][i])):
-            f.write(str("[Rule_no"+str(i)+":"+"\n"+"(?datafield1 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement1'][i]+"),"+"\n"+
-            "(?datafield2 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement2'][i]+"),"+"\n"+
-            "(?datafield1 dd:instrument ddinstrument:"+data['Sheet1']['Instrument1'][i]+"),"+"\n"+
-            "(?datafield2 dd:instrument ddinstrument:"+data['Sheet1']['Instrument2'][i]+"),"+"\n"+
-            "(?datafield1 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval1'][i]+"),"+"\n"+
-            "(?datafield2 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval2'][i]+"),"+"\n"+
-            "(?datafield1 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution1'][i]+"),"+"\n"+
-            "(?datafield2 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution2'][i]+"),"+"\n"
-            #+"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level1'][i]))+")"+"\n"+
-            +"(?datafield2 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level2'][i]))+"),"+"\n"
-            +"(?candidate dd:candidateEvent ?event),"+"\n"+
-            "(?event rdf:type dd:"+data['Sheet1']['Event+Type'][i]+"),"+"\n"+
-            "makeSkolem(?assertion, ?candidate, ?event, ?datafield1 , ?datafield2)"+"\n"
-            +"->"+"\n"+
-             "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
-            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
-            "(?assertion dd:compatibilityValue dd:"+compatibility+"\n"+
-            " (?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
-            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
-            +"]"+"\n"
-            ))
-        elif(np.isnan(data['Sheet1']['Processing+Level2'][i])):
-            f.write(str("[Rule_no"+str(i)+":"+"\n"+"(?datafield1 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement1'][i]+"),"+"\n"+
-            "(?datafield2 dd:measurement ddmeasurement:"+data['Sheet1']['Measurement2'][i]+"),"+"\n"+
-            "(?datafield1 dd:instrument ddinstrument:"+data['Sheet1']['Instrument1'][i]+"),"+"\n"+
-            "(?datafield2 dd:instrument ddinstrument:"+data['Sheet1']['Instrument2'][i]+"),"+"\n"+
-            "(?datafield1 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval1'][i]+"),"+"\n"+
-            "(?datafield2 dd:timeInterval ddtime:"+data['Sheet1']['Time_Interval2'][i]+"),"+"\n"+
-            "(?datafield1 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution1'][i]+"),"+"\n"+
-            "(?datafield2 dd:spatialResolution ddspatial:"+data['Sheet1']['Spatial_Resolution2'][i]+"),"+"\n"
-            +"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level1'][i]))+"),"+"\n"
-            #"(?datafield1 dd:processingLevel "+str(int(data['Sheet1']['Processing+Level2'][i]))+")"+"\n"
-            +"(?candidate dd:candidateEvent ?event),"+"\n"+
-            "(?event rdf:type dd:"+data['Sheet1']['Event+Type'][i]+"),"+"\n"+
-            "makeSkolem(?assertion, ?candidate, ?event, ?datafield1 , ?datafield2)"+"\n"
-            +"->"+"\n"+
-             "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
-            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
-            "(?assertion dd:compatibilityValue dd:"+compatibility+"\n"+
-            " (?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
-            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
-            +"]"+"\n"
-            ))
+#print(data['Sheet1'])
 
-#print(str(int(data['Sheet1']['Processing+Level2'][176])))
-#print(len(data['Sheet1']))
-print(type(data['Sheet1']['Correctness Average'][2]))
-i = 0
 f = open('Rules.txt','w+')
 f.write("@prefix dd: <http://www.purl.org/twc/ns/darkdata#>."+"\n"+
 "@prefix ddmeasurement: <http://darkdata.tw.rpi.edu/data/measurement/>."+"\n"+
 "@prefix ddtime: <http://darkdata.tw.rpi.edu/data/time-interval/>."+"\n"+
 "@prefix ddspatial:<http://darkdata.tw.rpi.edu/data/spatial-resolution/>."+"\n"+
 "@prefix ddinstrument:<http://darkdata.tw.rpi.edu/data/instrument/>."+"\n"+"\n")
+
+var = [['Measurement1','dd:measurement','ddmeasurement:'],['Measurement2','dd:measurement','ddmeasurement:'],['Instrument1','dd:instrument','ddinstrument:'],['Instrument2','dd:instrument','ddinstrument:'],['Time_Interval1','dd:timeInterval','ddtime:'],['Time_Interval2','dd:timeInterval','ddtime:'],['Spatial_Resolution1','dd:spatialResolution','ddspatial:'],['Spatial_Resolution2','dd:spatialResolution','ddspatial:'],['Processing+Level1','dd:processingLevel',''],['Processing+Level2','dd:processingLevel',''],['Event+Type','dd:candidateEvent','dd:']]
+
+
+def runR(compatibility,variable,i):
+    f.write("[Rule_no"+str(i)+":"+"\n")
+    for x in xrange(0,len(variable)-2,2):
+        if(not(str(data['Sheet1'][variable[x][0]][i])=='nan')):
+            f.write(str("(?datafield1 " + str(variable[x][1])+" " + str(variable[x][2])  +str(data['Sheet1'][variable[x][0]][i])+"),"+"\n"))
+        if(not(str(data['Sheet1'][variable[x+1][0]][i])=='nan')):
+            f.write(str("(?datafield2 " + str(variable[x][1])+" " + str(variable[x][2])  +str(data['Sheet1'][variable[x+1][0]][i])+"),"+"\n"))
+
+    if(not(str(data['Sheet1'][variable[len(variable)-1][0]][i])=='nan')):
+        f.write("(?candidate "+str(variable[len(variable)-1][1])+" ?event),"+"\n"+
+             "(?event rdf:type "+ str(variable[len(variable)-1][2]) +str(data['Sheet1'][variable[len(variable)-1][0]][i])+"),"+"\n")
+    f.write("makeSkolem(?assertion, ?candidate, ?datafield1 , ?datafield2)"+"\n")
+    f.write("->"+"\n")
+    f.write("(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
+            "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"+
+            "(?assertion dd:compatibilityValue dd:"+compatibility+"),"+"\n"+
+            "(?assertion dd:assertionConfidence \""+str(0.5)+"\"^^xsd:double)"+"\n"+
+            "(?assertion dd:basisForAssertion <urn:rule/Rules.txt/<Rule_no"+str(i)+">)"+"\n"
+            +"]"+"\n"
+            )
 for i in range(0,len(data['Sheet1'])):
     print i
     if(data['Sheet1']['Correctness Average'][i]>4.0 and data['Sheet1']['Correctness Average'][i]<=5.0):
-        runrules('strong_compatibility')
+        runR('strong_compatibility',var,i)
     elif(data['Sheet1']['Correctness Average'][i]>3.0 and data['Sheet1']['Correctness Average'][i]<=4.0):
-        runrules("some_compatibility")
+        runR("some_compatibility",var,i)
     elif(data['Sheet1']['Correctness Average'][i]>2.0 and data['Sheet1']['Correctness Average'][i]<=3.0):
-        runrules("slight_compatibility")
+        runR("slight_compatibility",var,i)
     elif(data['Sheet1']['Correctness Average'][i]>1.0 and data['Sheet1']['Correctness Average'][i]<=2.0):
-        runrules("indifferent_compatibility")
+        runR("indifferent_compatibility",var,i)
     else:
-        runrules("negative_compatibility")
+        runR("negative_compatibility",var,i)
+
+
+f.close()
+
