ca5046346b5e2204b2de5909f0f97434221a0fa2
anirudhprabhu <anirudh.prabhu@gmail.com>
2016-05-11 13:22:58 -0400

    Created new rules files for each SME

--- DarkData_Python/Rules_from_experts/Measurement_Rules.py
+++ DarkData_Python/Rules_from_experts/Measurement_Rules.py
@@ -4,9 +4,10 @@ import xlrd
 Rules = {}
 
 with pd.ExcelFile('X Applicability to Phenomena from experts.xlsx') as xls:
-    Rules['Spatial_Resolution'] = pd.read_excel(xls, 'Spatial Resolution', skiprows=[8,9,10], index_col=None, na_values=['NA'])
-    Rules['Measurement'] = pd.read_excel(xls, 'Measurement_shen', skiprows=[8,9,10,11],index_col=0,na_values=['NA'])
-
+    Rules['Spatial_Resolution_Li'] = pd.read_excel(xls, 'Spatial Resolution', skiprows=[8,9,10], index_col=None, na_values=['NA'])
+    Rules['Measurement_Li'] = pd.read_excel(xls, 'Measurement', skiprows=[8,9,10,11],index_col=0,na_values=['NA'])
+    Rules['Spatial_Resolution_Shen'] = pd.read_excel(xls, 'Resolution_shen', skiprows=[8,9,10], index_col=None, na_values=['NA'])
+    Rules['Measurement_Shen'] = pd.read_excel(xls, 'Measurement_shen', skiprows=[8,9,10,11],index_col=0,na_values=['NA'])
 
 #print(Rules['Spatial_Resolution'])
 #print(Rules['Measurement']['Sensible Heat Flux'])
@@ -14,9 +15,9 @@ with pd.ExcelFile('X Applicability to Phenomena from experts.xlsx') as xls:
 measurements = ['Sensible Heat Flux','Wind Stress Direction','Latent Heat Flux','Wind','Albedo','Wind Velocity','Dust','NO2','CO2','Incident Radiation','Ground Heat','Statistics','Geopotential','Soil Temperature','Heat Flux','Soil Moisture']
 events = ['Hurricane','TropicalStorm','VolcanicEruption','Flood','Fire','DustStorm','Drought']
 
-def fun(i,x):
+def fun(i,x,sheetname):
 
-    y = (str(Rules['Measurement'][measurements[x]][i]))
+    y = (str(Rules[sheetname][measurements[x]][i]))
     yy = y.split(' ')
 
     if(yy[0].upper()=='GREAT'):
@@ -49,25 +50,52 @@ def fun(i,x):
 
     #f.write('(?assertion dd:assertionConfidence \"'+yy[1]+'\"^^xsd:double),'+"\n")
     #f.write('(?assertion dd:assertionConfidence \"'+"0.5"+'\"^^xsd:double),'+"\n")
-f = open('measurement.rules','w')
+f = open('measurement_Li.rules','w')
 f.write('@prefix dd: <http://www.purl.org/twc/ns/darkdata#>.'+'\n'+
     '@prefix ddmeasurement: <http://darkdata.tw.rpi.edu/data/measurement/>.'+"\n")
+
+sheetname1 = 'Measurement_Li'
 
 for i in range(0,len(events)):
     f.write("\n"+"#"+events[i]+"\n")
     for j in range(0,len(measurements)):
-        f.write("["+events[i]+"-"+str(Rules['Measurement'][measurements[j]].name).replace(' ','+')+":"+"\n")
+        f.write("["+events[i]+"-"+str(Rules[sheetname1][measurements[j]].name).replace(' ','+')+":"+"\n")
         f.write("(?candidate dd:candidateEvent ?event),"+"\n"+
         "(?event rdf:type dd:"+events[i]+"),"+"\n"+
         "(?candidate dd:candidateVariable ?variable),"+"\n"+
-        "(?variable dd:measurement ddmeasurement:"+str(Rules['Measurement'][measurements[j]].name).replace(' ','+')+"),"+"\n"+
-        "makeSkolem(?assertion, ?candidate, ?event, dd:"+events[i]+" ,"+"ddmeasurement:"+str(Rules['Measurement'][measurements[j]].name).replace(' ','+')+", ?variable)" +"\n"+
+        "(?variable dd:measurement ddmeasurement:"+str(Rules[sheetname1][measurements[j]].name).replace(' ','+')+"),"+"\n"+
+        "makeSkolem(?assertion, ?candidate, ?event, dd:"+events[i]+" ,"+"ddmeasurement:"+str(Rules[sheetname1][measurements[j]].name).replace(' ','+')+", ?variable)" +"\n"+
         "->"+"\n"+
         "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
         "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"
         )
-        fun(i,j)
-        f.write("(?assertion dd:basisForAssertion <urn:rule/measurement/"+events[i]+"-"+str(Rules['Measurement'][measurements[j]].name).replace(' ','+')+">"+")"+"\n"+"]"+"\n")
+        fun(i,j,sheetname1)
+        f.write("(?assertion dd:basisForAssertion <urn:rule/measurement/"+events[i]+"-"+str(Rules[sheetname1][measurements[j]].name).replace(' ','+')+">"+")"+"\n"+"]"+"\n")
 
     f.write('\n')
+f.close()
 
+
+f = open('measurement_Shen.rules','w')
+f.write('@prefix dd: <http://www.purl.org/twc/ns/darkdata#>.'+'\n'+
+    '@prefix ddmeasurement: <http://darkdata.tw.rpi.edu/data/measurement/>.'+"\n")
+
+sheetname2 = 'Measurement_Shen'
+for i in range(0,len(events)):
+    f.write("\n"+"#"+events[i]+"\n")
+    for j in range(0,len(measurements)):
+        f.write("["+events[i]+"-"+str(Rules[sheetname2][measurements[j]].name).replace(' ','+')+":"+"\n")
+        f.write("(?candidate dd:candidateEvent ?event),"+"\n"+
+        "(?event rdf:type dd:"+events[i]+"),"+"\n"+
+        "(?candidate dd:candidateVariable ?variable),"+"\n"+
+        "(?variable dd:measurement ddmeasurement:"+str(Rules[sheetname2][measurements[j]].name).replace(' ','+')+"),"+"\n"+
+        "makeSkolem(?assertion, ?candidate, ?event, dd:"+events[i]+" ,"+"ddmeasurement:"+str(Rules[sheetname2][measurements[j]].name).replace(' ','+')+", ?variable)" +"\n"+
+        "->"+"\n"+
+        "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
+        "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"
+        )
+        fun(i,j,sheetname2)
+        f.write("(?assertion dd:basisForAssertion <urn:rule/measurement/"+events[i]+"-"+str(Rules[sheetname2][measurements[j]].name).replace(' ','+')+">"+")"+"\n"+"]"+"\n")
+
+    f.write('\n')
+f.close()
--- DarkData_Python/Rules_from_experts/Spatial_Resolution_Rules.py
+++ DarkData_Python/Rules_from_experts/Spatial_Resolution_Rules.py
@@ -4,8 +4,10 @@ import xlrd
 Rules = {}
 
 with pd.ExcelFile('X Applicability to Phenomena from experts.xlsx') as xls:
-    Rules['Spatial_Resolution'] = pd.read_excel(xls, 'Spatial Resolution', skiprows=[8,9,10], index_col=None, na_values=['NA'])
-    Rules['Measurement'] = pd.read_excel(xls, 'Measurement', skiprows=[8,9,10,11],index_col=0,na_values=['NA'])
+    Rules['Spatial_Resolution_Li'] = pd.read_excel(xls, 'Spatial Resolution', skiprows=[8,9,10], index_col=None, na_values=['NA'])
+    Rules['Measurement_Li'] = pd.read_excel(xls, 'Measurement', skiprows=[8,9,10,11],index_col=0,na_values=['NA'])
+    Rules['Spatial_Resolution_Shen'] = pd.read_excel(xls, 'Resolution_shen', skiprows=[8,9,10], index_col=None, na_values=['NA'])
+    Rules['Measurement_Shen'] = pd.read_excel(xls, 'Measurement_shen', skiprows=[8,9,10,11],index_col=0,na_values=['NA'])
 
 
 #print(Rules['Spatial_Resolution'])
@@ -14,9 +16,9 @@ with pd.ExcelFile('X Applicability to Phenomena from experts.xlsx') as xls:
 spatialRes = ['2 x 2.5 deg.','1.0 x 1.25 deg.','1 x 1.25 deg.','0.5 x 0.625 deg.','0.125 deg.','1 deg.','0.1 deg.','0.667 x 1.25 deg.','0.5 deg.','5000 m','0.05 deg.','5600 m','0.25 deg.','0.5 x 0.667 deg.','1.25 deg.']
 events = ['Hurricane','TropicalStorm','VolcanicEruption','Flood','Fire','DustStorm','Drought']
 
-def fun(i,x):
+def fun(i,x,sheetname):
 
-    y = (str(Rules['Spatial_Resolution'][spatialRes[x]][i]))
+    y = (str(Rules[sheetname][spatialRes[x]][i]))
     yy = y.split(' ')
 
     if(yy[0].upper()=='GREAT'):
@@ -49,25 +51,50 @@ def fun(i,x):
 
     #f.write('(?assertion dd:assertionConfidence \"'+yy[1]+'\"^^xsd:double),'+"\n")
     #f.write('(?assertion dd:assertionConfidence \"'+"0.5"+'\"^^xsd:double),'+"\n")
-f = open('spatial_res.rules','w')
+f = open('spatial_res_Li.rules','w')
 f.write('@prefix dd: <http://www.purl.org/twc/ns/darkdata#>.'+'\n'+
     '@prefix ddspatial:<http://darkdata.tw.rpi.edu/data/spatial-resolution/>.'+"\n")
+
+sheetname1 = 'Spatial_Resolution_Li'
 
 for i in range(0,len(events)):
     f.write("\n"+"#"+events[i]+"\n")
     for j in range(0,len(spatialRes)):
-        f.write("["+events[i]+"-"+str(Rules['Spatial_Resolution'][spatialRes[j]].name).replace(' ','+')+":"+"\n")
+        f.write("["+events[i]+"-"+str(Rules[sheetname1][spatialRes[j]].name).replace(' ','+')+":"+"\n")
         f.write("(?candidate dd:candidateEvent ?event),"+"\n"+
         "(?event rdf:type dd:"+events[i]+"),"+"\n"+
         "(?candidate dd:candidateVariable ?variable),"+"\n"+
-        "(?variable dd:spatialResolution ddspatial:"+str(Rules['Spatial_Resolution'][spatialRes[j]].name).replace(' ','+')+"),"+"\n"+
-        "makeSkolem(?assertion, ?candidate, ?event, dd:"+events[i]+", "+"ddspatial:"+str(Rules['Spatial_Resolution'][spatialRes[j]].name).replace(' ','+')+" ,?variable)" +"\n"+
+        "(?variable dd:spatialResolution ddspatial:"+str(Rules[sheetname1][spatialRes[j]].name).replace(' ','+')+"),"+"\n"+
+        "makeSkolem(?assertion, ?candidate, ?event, dd:"+events[i]+", "+"ddspatial:"+str(Rules[sheetname1][spatialRes[j]].name).replace(' ','+')+" ,?variable)" +"\n"+
         "->"+"\n"+
         "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
         "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"
         )
-        fun(i,j)
-        f.write("(?assertion dd:basisForAssertion <urn:rule/spatial_res/"+events[i]+"-"+str(Rules['Spatial_Resolution'][spatialRes[j]].name).replace(' ','+')+">)"+"\n"+"]"+"\n")
+        fun(i,j,sheetname1)
+        f.write("(?assertion dd:basisForAssertion <urn:rule/spatial_res/"+events[i]+"-"+str(Rules[sheetname1][spatialRes[j]].name).replace(' ','+')+">)"+"\n"+"]"+"\n")
 
     f.write('\n')
 
+f.close()
+
+f = open('spatial_res_shen.rules','w')
+f.write('@prefix dd: <http://www.purl.org/twc/ns/darkdata#>.'+'\n'+
+    '@prefix ddspatial:<http://darkdata.tw.rpi.edu/data/spatial-resolution/>.'+"\n")
+sheetname2 = 'Spatial_Resolution_Shen'
+for i in range(0,len(events)):
+    f.write("\n"+"#"+events[i]+"\n")
+    for j in range(0,len(spatialRes)):
+        f.write("["+events[i]+"-"+str(Rules[sheetname2][spatialRes[j]].name).replace(' ','+')+":"+"\n")
+        f.write("(?candidate dd:candidateEvent ?event),"+"\n"+
+        "(?event rdf:type dd:"+events[i]+"),"+"\n"+
+        "(?candidate dd:candidateVariable ?variable),"+"\n"+
+        "(?variable dd:spatialResolution ddspatial:"+str(Rules[sheetname2][spatialRes[j]].name).replace(' ','+')+"),"+"\n"+
+        "makeSkolem(?assertion, ?candidate, ?event, dd:"+events[i]+", "+"ddspatial:"+str(Rules[sheetname2][spatialRes[j]].name).replace(' ','+')+" ,?variable)" +"\n"+
+        "->"+"\n"+
+        "(?candidate dd:compatibilityAssertion ?assertion),"+"\n"+
+        "(?assertion rdf:type dd:CompatibilityAssertion),"+"\n"
+        )
+        fun(i,j,sheetname2)
+        f.write("(?assertion dd:basisForAssertion <urn:rule/spatial_res/"+events[i]+"-"+str(Rules[sheetname2][spatialRes[j]].name).replace(' ','+')+">)"+"\n"+"]"+"\n")
+
+    f.write('\n')
