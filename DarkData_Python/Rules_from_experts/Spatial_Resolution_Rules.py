1ed9e1e19c1b677b02fe65f25f0d7906fa63a081
anirudhprabhu <anirudh.prabhu@gmail.com>
2016-05-13 13:04:21 -0400

    Changed 'Fire' to 'Wildfire' and modified script accordingly

--- DarkData_Python/Rules_from_experts/Spatial_Resolution_Rules.py
+++ DarkData_Python/Rules_from_experts/Spatial_Resolution_Rules.py
@@ -14,7 +14,7 @@ with pd.ExcelFile('X Applicability to Phenomena from experts.xlsx') as xls:
 #print(Rules['Measurement'])
 
 spatialRes = ['2 x 2.5 deg.','1.0 x 1.25 deg.','1 x 1.25 deg.','0.5 x 0.625 deg.','0.125 deg.','1 deg.','0.1 deg.','0.667 x 1.25 deg.','0.5 deg.','5000 m','0.05 deg.','5600 m','0.25 deg.','0.5 x 0.667 deg.','1.25 deg.']
-events = ['Hurricane','TropicalStorm','VolcanicEruption','Flood','Fire','DustStorm','Drought']
+events = ['Hurricane','TropicalStorm','VolcanicEruption','Flood','Wildfire','DustStorm','Drought']
 
 def fun(i,x,sheetname):
 
